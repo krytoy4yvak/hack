c1d5927cc7ca76f08c6d6874973275bf3182c72e
Вера Аношина <veraanosina@Veras-MacBook-Air.local>
2020-09-20 01:14:42 +0300

    commite

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import sys
+import re
+import joblib
+
+
 def input_one(input = None):
-    return input
+    file = open(input[1], 'r')
+    lines = file.read()
+    file.close()
+    text = ' '.join(re.sub(r'[^a-zA-Zа-яА-Я ]', '', ' '.join(str(lines).lower().split())).split())
+    print(text)
+    model = joblib.load('models/validation/logreg_cv.joblib')
+    cv = joblib.load('models/validation/count_vec.joblib')
+    predict = model.predict(cv.transform([text]))
+    return predict
 
 
 if __name__ == '__main__':
-    input_one(sys.argv)
+    print(input_one(sys.argv))
